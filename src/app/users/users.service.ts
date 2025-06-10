f14679838b959213eef967769cd298ed88931010
Marax-github <66254363+Marax-github@users.noreply.github.com>
2025-06-10 15:12:10 +0200

    Apply User interface typing

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "./users.model";
 
 
 @Injectable({providedIn:"root"})
 export class UserService {
 
-    private users = new BehaviorSubject([
+    private users = new BehaviorSubject<User[]>([
         {name:'John Doe', email:'John@doe.com',phone:'123-456-789',address:'John Doe Street',status:'Active'},
         {name: 'Jane Smith',email: 'jane@example.com',phone: '987-654-321',address: '456 Elm St',status: 'Inactive'},
     ]);
-      
-    public user = this.users.asObservable();
 
-    addUser(user: any){
+    public users$ = this.users.asObservable();
+
+    addUser(user: User){
         const currentUser = this.users.getValue();
         console.log(this.users);
         this.users.next([...currentUser, user])
@@ -24,14 +25,14 @@ export class UserService {
         this.users.next(currentUser.filter((u) => u.email !== email));
     }
 
-    editUser(updateUser: any){
+    editUser(updateUser: Partial<User>){
         const users = this.users.getValue().map(user =>
             user.email === updateUser.email ? {...user, ...updateUser} : user
         );
         this.users.next(users);
     }
-    
-    getUsers(): Observable<any[]>{
+
+    getUsers(): Observable<User[]>{
         return this.users.asObservable();
     }
 }
